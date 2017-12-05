845acb7dbe4603406f4b596f9c00c60b77af55da
Андрей Петров <andigpetrov@gmail.com>
2017-12-05 12:24:20 +0300

    aaa

--- MyBookshelfData/User.cs
+++ MyBookshelfData/User.cs
@@ -8,5 +8,13 @@ namespace MyBookshelfData
 {
     class User
     {
+        public int Id { get; set; }
+
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+
+        public DateTime Birth { get; set; }
+
     }
 }
