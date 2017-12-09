1b43d9cf2e3caf65aab6545f9ec733da1c6482e0
Владислава Коковкина <kokovkina89@mail.ru>
2017-12-09 16:07:45 +0300

    Markup is made, approximate UI

--- MyBookshelf/Authorization.xaml.cs
+++ MyBookshelf/Authorization.xaml.cs
@@ -1,5 +1,4 @@
-﻿using MyBookshelfData;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,22 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace MyBookshelf
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для Authorization.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Authorization : Window
     {
-        public MainWindow()
+        public Authorization()
         {
             InitializeComponent();
-
-            Context context = new Context();
-            context.Books.ToList();
         }
     }
 }
